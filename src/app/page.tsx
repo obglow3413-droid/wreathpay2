6d5b02ddaa886ba6799eaa7de0cf8425f9f60ca6
obglow3413-droid <obglow3413@gmail.com>
2026-08-31 23:37:53 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import AppTopBar from "@/components/home/AppTopBar";
 import AppBottomNav from "@/components/layout/AppBottomNav";
 import SiteFooter from "@/components/layout/SiteFooter";
 import Hero from "@/components/home/Hero";
-import ServiceSelector from "@/components/home/ServiceSelector";
+import LinkShareSection from "@/components/home/LinkShareSection";
 import TrustIndicators from "@/components/home/TrustIndicators";
 import ValueSection from "@/components/home/ValueSection";
 import LiveFeed from "@/components/home/LiveFeed";
@@ -27,7 +27,7 @@ export default function HomePage() {
       <AppTopBar />
       <main className="flex-1 pb-24">
         <Hero />
-        <ServiceSelector />
+        <LinkShareSection />
         <TrustIndicators />
         <Reveal>
           <ValueSection />
